0e167e85f8c2ff0e75c943c434e792dfa064f6dd
Alex Papadopulos <alex.j.devil@gmail.com>
2021-11-29 19:11:37 +0000

    second push

---
+++ anything.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Hello everyone, I'm Alex and I'm in mars...")
+
+	fooGo()
+
+	fmt.Println("something more please!!")
+
+}
+
+func fooGo() {
+	fmt.Println("I'm in the loo doing some foo go baby!!")
+}
